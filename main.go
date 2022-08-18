a9a02f40713a2ad942528339fa9656dc2ad2ef03
eyupfidan <eyupfidanweb@gmail.com>
2022-08-18 09:37:27 +0300

    add deleteMovie  function

--- main.go
+++ main.go
@@ -26,6 +26,16 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 
 }
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
 
 var movies []Movie
 
